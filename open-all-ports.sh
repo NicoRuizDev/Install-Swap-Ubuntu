ba82b0e4bb5ad5a8438147f59479e66c193db4a4
Nico Ruiz <lifegamer2050@gmail.com>
2023-10-30 18:16:00 +0530

    Update open-all-ports.sh

--- open-all-ports.sh
+++ open-all-ports.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+# Check if the script is run as root
 if [ "$EUID" -ne 0 ]; then
   echo "This script needs to be run as root."
   exit 1
 fi
 
+# Ask for confirmation
 read -p "This script will reset your iptables rules to default and save them to ~/iptables-rules. Do you want to continue? (y/n): " confirm
 
 if [ "$confirm" != "y" ] && [ "$confirm" != "Y" ]; then
@@ -10,6 +14,7 @@ if [ "$confirm" != "y" ] && [ "$confirm" != "Y" ]; then
   exit 1
 fi
 
+# Reset iptables rules and save to ~/iptables-rules
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
@@ -18,4 +23,5 @@ sudo iptables-save > ~/iptables-rules
 
 echo "Successfully done."
 
-echo "Check out my GitHub repository for more scripts and projects: https://github.com/NicoRuizDev/Scripts"
+# Promote your GitHub repo
+echo "Check out my GitHub repository for more scripts and projects: https://github.com/NicoRuizDev"
